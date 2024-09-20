d40322c31d5df920d328d63dafa828559a7c47f1
Guilherme Coelho <valadaogui2003@gmail.com>
2024-09-20 17:30:53 -0300

    add historico GUI

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 class MyApp:
     def __init__(self):
@@ -21,30 +22,38 @@ class MyApp:
     def __widgets(self):
         txt_title = Label(self.window, text='Conversor MP3', fg='#fff', bg='#1E1E1E', font=("Arial", 15) ).place(x=200, y=30)
         #URL#
-        self.txt_url = Label(self.window, text='URL:', fg='#fff', bg='#1E1E1E', font=("Arial", 12), ).place(x=80, y=80)
-        self.ipt_url = Entry(self.window, width=60,textvariable=self.var_url)
-        self.ipt_url.place(x=140, y=80)
+        txt_url = Label(self.window, text='URL:', fg='#fff', bg='#1E1E1E', font=("Arial", 12), ).place(x=80, y=80)
+        ipt_url = Entry(self.window, width=60,textvariable=self.var_url)
+        ipt_url.place(x=140, y=80)
         #BTNs#
         btn_baixar = Button(self.window, text='Baixar', width=20, height=1, cursor='hand2')
-        btn_baixar.place(x=180, y=120)
-        self.btn_histo = Button(self.window, text='Historico', width=20, height=1, cursor='hand2', command=self.__historico)
-        self.btn_histo.place(x=300, y=120)
-        #Historico#
+        btn_baixar.place(x=120, y=120)
+        btn_histo = Button(self.window, text='Historico', width=20, height=1, cursor='hand2', command=self.__historico)
+        btn_histo.place(x=320, y=120)
+        btn_sair = Button(self.window, text='Sair', width=10, height=1, cursor='hand2', command=self.sair)
+        btn_sair.place(x=450, y=280)
         
 
     def __historico(self):
         window_2 = Tk()
-        self.table = ttk.Treeview(window_2,selectmode='browse', columns=('coluna0','coluna1', 'coluna2'), show='headings')
-        self.table.place(x=100, y=150)
-        self.table.column('coluna0', width=60, minwidth=60, stretch=NO)
-        self.table.heading('#1', text='ID')
-        self.table.column('coluna1', width=190, minwidth=120, stretch=NO)
-        self.table.heading('#2', text='NOME')
-        self.table.column('coluna2', width=140, minwidth=120, stretch=NO)
-        self.table.heading('#3', text='DATA')
+        window_2.geometry('430x260')
+        table = ttk.Treeview(window_2,selectmode='browse', columns=('coluna0','coluna1', 'coluna2'), show='headings')
+        table.place(x=17, y=20)
+        table.column('coluna0', width=60, minwidth=60, stretch=NO)
+        table.heading('#1', text='ID')
+        table.column('coluna1', width=190, minwidth=120, stretch=NO)
+        table.heading('#2', text='NOME')
+        table.column('coluna2', width=140, minwidth=120, stretch=NO)
+        table.heading('#3', text='DATA')
+
+
         window_2.mainloop()
+
     def __loop(self):
         self.window.mainloop()
 
+    def sair(self):
+        if messagebox.askyesno('Sair', 'Deseja sair?'):
+            self.window.destroy()
     
 MyApp()
