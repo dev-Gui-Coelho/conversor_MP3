c5a9203717c299edefed5e50c7ee1d40558b651a
Guilherme Coelho <valadaogui2003@gmail.com>
2024-09-20 18:07:32 -0300

    armazenamento do var url

--- conversor.py
+++ conversor.py
@@ -1,14 +1,19 @@
 import os 
 from pytube import YouTube
 from moviepy.editor import *
-import time
+from index import *
 
-#  video = YouTube(values['url'])
-#                 steam = video.streams.get_highest_resolution().download()
-#                 name = steam.replace("D:\Codigos\Baixar_Musicas\\", '')
-#                 name = steam.replace(".mp4", '')
-#                 file = AudioFileClip(f'{steam}')
-#                 file.write_audiofile(f'{name}.mp3')
-#                 file.close()
-#                 os.unlink(steam)
-#                 os.unlink(f'{name}.mp3')
+url = MyApp.url
+def teste(url):
+    print(url)
+
+    
+video = YouTube(url)
+steam = video.streams.get_highest_resolution().download()
+name = steam.replace("D:\Codigos\Baixar_Musicas\\", '')
+name = steam.replace(".mp4", '')
+file = AudioFileClip(f'{steam}')
+file.write_audiofile(f'{name}.mp3')
+file.close()
+os.unlink(steam)
+os.unlink(f'{name}.mp3')
--- index.py
+++ index.py
@@ -3,38 +3,44 @@ from tkinter import ttk
 from tkinter import messagebox
 
 class MyApp:
-    def __init__(self):
+    def __init__(self, url:str = None):
+        self.__url = url
         self.__create_window()
         self.__var()
         self.__widgets()
         self.__loop()
 
-    def __create_window(self):
+    def __create_window(self) -> None:
         self.window = Tk()
         self.window.geometry("550x328")
         self.window.resizable(False,False)
         self.window.config(bg='#1E1E1E')
         self.window.title('Conversor MP3')
         
-    def __var(self):
+    def __var(self) -> str:
         self.var_url = StringVar(self.window)
-        # print(self.var_url.get())
+        self.url = self.var_url
+        return self.url
+    
     def __widgets(self):
         txt_title = Label(self.window, text='Conversor MP3', fg='#fff', bg='#1E1E1E', font=("Arial", 15) ).place(x=200, y=30)
         #URL#
+        # var_url = StringVar(self.window)
+        # self.url = var_url
         txt_url = Label(self.window, text='URL:', fg='#fff', bg='#1E1E1E', font=("Arial", 12), ).place(x=80, y=80)
-        ipt_url = Entry(self.window, width=60,textvariable=self.var_url)
-        ipt_url.place(x=140, y=80)
+        self.ipt_url = Entry(self.window, width=60,textvariable=self.var_url)
+        self.ipt_url.place(x=140, y=80)
         #BTNs#
-        btn_baixar = Button(self.window, text='Baixar', width=20, height=1, cursor='hand2')
+        btn_baixar = Button(self.window, text='Baixar', width=20, height=1, cursor='hand2', command=self.__baixar)
         btn_baixar.place(x=120, y=120)
         btn_histo = Button(self.window, text='Historico', width=20, height=1, cursor='hand2', command=self.__historico)
         btn_histo.place(x=320, y=120)
         btn_sair = Button(self.window, text='Sair', width=10, height=1, cursor='hand2', command=self.sair)
         btn_sair.place(x=450, y=280)
         
+    
 
-    def __historico(self):
+    def __historico(self) -> None:
         window_2 = Tk()
         window_2.geometry('430x260')
         table = ttk.Treeview(window_2,selectmode='browse', columns=('coluna0','coluna1', 'coluna2'), show='headings')
@@ -46,14 +52,17 @@ class MyApp:
         table.column('coluna2', width=140, minwidth=120, stretch=NO)
         table.heading('#3', text='DATA')
 
-
         window_2.mainloop()
 
-    def __loop(self):
+    def __loop(self) -> None:
         self.window.mainloop()
 
-    def sair(self):
+    def sair(self) -> None:
         if messagebox.askyesno('Sair', 'Deseja sair?'):
             self.window.destroy()
     
+    def __baixar(self):
+        
+        print(self.url.get())
+
 MyApp()
